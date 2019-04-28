e6bf2d811dc53987f237ba5220dfa71d50e2b146
pacojq <pacojuanquiros@gmail.com>
2019-04-28 23:50:29 +0200

    SDI-SW-11.2 Servicio SOAP Cliente

---
+++ src/main/java/com/uniovi/Sdi708LabSoapClienteApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi708LabSoapClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sdi708LabSoapClienteApplication.class, args);
+	}
+
+}
